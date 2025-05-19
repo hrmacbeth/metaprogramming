dc51d12ee757690311525e463ecc992f868e93c9
Heather Macbeth <25316162+hrmacbeth@users.noreply.github.com>
2025-05-19 13:18:44 +0100

    test file for cancel

---
+++ Metaprogramming/Cancel/Test.lean
@@ -0,0 +1,12 @@
+import Mathlib
+
+/-! ### Lemmas to tag @[cancel] -/
+
+#check lt_of_add_lt_add_right
+
+/-! ### Tests -/
+
+example {a b x y : ℝ} (h : 0 ≤ a) (h : a * x + b < a * y + b) : True := by
+  apply lt_of_add_lt_add_right at h
+  replace h := lt_of_mul_lt_mul_left h (by positivity)
+  trivial
